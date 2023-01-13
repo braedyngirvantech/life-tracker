2bbbcc8788998e2681ae9911412c5217e1ee9aec
Braedyn Girvan <braedyn.girvan.tech@gmail.com>
2023-01-13 23:27:58 +1300

    Update list menu

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -4,11 +4,13 @@ import './Toolbar.css';
 function Toolbar(){
     return <div className='toolBar'>
         <img src={logo} className="toolbar-logo" alt="logo" href='/PurchaseTracker.js'/>
-        <a href="/PurchaseTracker.js">Purchase Tracker</a>
-        <a>Food Tracker</a>
-        <button>Hygiene Tracker</button>
-        <button>Debt Tracker</button>
-        <button>Lending Tracker</button>
+        <ul>
+        <li><a href="/PurchaseTracker.js">Purchase Tracker</a></li>
+        <li><a>Food Tracker</a></li>
+        <li><a>Hygiene Tracker</a></li>
+        <li><a>Debt Tracker</a></li>
+        <li><a>Lending Tracker</a></li>
+        </ul>
     </div>;
 }
 
